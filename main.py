5abb71990327ca68048802a931a8162c04021424
ioannisxar <zannisxarchakos@hotmail.com>
2016-11-19 16:54:32 +0200

    main testing

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from src.feature_extraction.simple_stats import calculate_winner_range
+from src.feature_extraction.points_features import calculate_home_average_points
 import pandas
-data_frame = pandas.read_csv('data/nba_games_2004_2005.csv')
-for index, row in data_frame.iterrows():
-    print index, calculate_winner_range(row['home_points'], row['away_points'])
+
+
+if __name__ == "__main__":
+    # dicts
+    total_home_games_dict = dict()
+    total_home_points_dict = dict()
+
+    data_frame = pandas.read_csv('data/nba_games_2004_2005.csv')
+    for index, row in data_frame.iterrows():
+        # print index, calculate_winner_range(row['home_points'], row['away_points'])
+        total_home_games_dict, total_home_points_dict, percentage = \
+            calculate_home_average_points(total_home_games_dict, total_home_points_dict,
+                                          row['home_team'], row['home_points'])
+        print row['home_team'], percentage
--- src/feature_extraction/points_features.py
+++ src/feature_extraction/points_features.py
@@ -13,10 +13,10 @@ def calculate_home_average_points(total_home_games_dict, total_home_points_dict,
         total_home_games_dict[team_name] = 1
 
     if team_name in total_home_points_dict:
-        total_home_points_dict += home_points
+        total_home_points_dict[team_name] += home_points
     else:
         total_home_points_dict[team_name] = home_points
 
-    percentage = float(total_home_points_dict)/float(total_home_games_dict)
+    percentage = float(total_home_points_dict[team_name])/float(total_home_games_dict[team_name])
 
     return total_home_games_dict, total_home_points_dict, percentage
