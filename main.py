86287821213ef46ad3c25d2e433de9cbc9387f32
ioannisxar <zannisxarchakos@hotmail.com>
2016-11-19 15:43:51 +0200

    home points percentage feature

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-from feature_extraction import simple_stats
+from src.feature_extraction.simple_stats import calculate_winner_range
+import pandas
+data_frame = pandas.read_csv('data/nba_games_2004_2005.csv')
+for index, row in data_frame.iterrows():
+    print index, calculate_winner_range(row['home_points'], row['away_points'])
---
+++ src/feature_extraction/points_features.py
@@ -0,0 +1,22 @@
+def calculate_home_average_points(total_home_games_dict, total_home_points_dict, team_name, home_points):
+    """
+    :param total_home_games_dict:
+    :param total_home_points_dict:
+    :param team_name:
+    :param home_points:
+
+    :return:
+    """
+    if team_name in total_home_games_dict:
+        total_home_games_dict[team_name] += 1
+    else:
+        total_home_games_dict[team_name] = 1
+
+    if team_name in total_home_points_dict:
+        total_home_points_dict += home_points
+    else:
+        total_home_points_dict[team_name] = home_points
+
+    percentage = float(total_home_points_dict)/float(total_home_games_dict)
+
+    return total_home_games_dict, total_home_points_dict, percentage
