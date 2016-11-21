635f184d0f9bfe5e564fe1c10584079326efde11
ioannisxar <zannisxarchakos@hotmail.com>
2016-11-21 19:30:08 +0200

    [FIX] missing data produced nan values [FIX] feature matrix to csv

--- src/tools/general_tools.py
+++ src/tools/general_tools.py
@@ -18,9 +18,9 @@ def write_to_csv(final_dictionary):
     """
     with open("data/machine_learning/feature_matrix.csv", 'wb') as outfile:
         writer = csv.writer(outfile)
-        print final_dictionary.keys()
-        print type(final_dictionary.keys())
-        writer.writerow(np.array(zip(*[final_dictionary.keys()])).transpose())
-        writer.writerows(np.array(zip(*final_dictionary.values())).transpose())
-
-
+        for k, v in final_dictionary.iteritems():
+            l = list()
+            l.append(k)
+            for v1 in v:
+                l.append(v1)
+            writer.writerow(l)
