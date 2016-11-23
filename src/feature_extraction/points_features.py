570cb1e8e69f3326dddb96e5ed0972c25d1ba23e
ioannisxar <zannisxarchakos@hotmail.com>
2016-11-24 00:16:06 +0200

    [DEV] calculate_average_points_allowed feature

--- src/feature_extraction/points_features.py
+++ src/feature_extraction/points_features.py
@@ -22,6 +22,12 @@ def points_features(data_frame):
     dict_list.append(avg_points[1])
     feature_name_list.append("average_points_away")
 
+    # average allowed points per game
+    dict_list.append(calculate_average_points_allowed(data_frame, "home"))
+    feature_name_list.append("calculate_average_home_allowed_points_home")
+    dict_list.append(calculate_average_points_allowed(data_frame, "away"))
+    feature_name_list.append("calculate_average_away_allowed_points_away")
+
     return dict_list, feature_name_list
 
 
@@ -99,3 +105,36 @@ def averages(data_frame, feature):
                     total_dict[row[team_name]] = row[feature+team_name.split('_')[0]]
 
     return percentage_home_dict, percentage_away_dict
+
+
+def calculate_average_points_allowed(data_frame, mode):
+    """
+    :param data_frame: The loaded input file
+    :param mode:
+    :return:
+    """
+    total_games_dict, total_points_dict, percentage_dict = dict(), dict(), dict()
+    if mode == 'home':
+        team_name = 'home_team'
+        opponent_points = 'away_points'
+    else:
+        team_name = 'away_team'
+        opponent_points = 'home_points'
+
+    for index, row in data_frame.iterrows():
+        if row[team_name] not in total_games_dict:
+            percentage_dict[row["id"]] = 0
+        else:
+            percentage_dict[row["id"]] = format(float(total_points_dict[row[team_name]]) / float(total_games_dict[row[team_name]]), '.2f')
+
+        if row[team_name] in total_games_dict:
+            total_games_dict[row[team_name]] += 1
+        else:
+            total_games_dict[row[team_name]] = 1
+
+        if row[team_name] in total_points_dict:
+            total_points_dict[row[team_name]] += row[opponent_points]
+        else:
+            total_points_dict[row[team_name]] = row[opponent_points]
+
+    return percentage_dict
